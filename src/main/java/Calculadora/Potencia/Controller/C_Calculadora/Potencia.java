c0941549c180088db004fdea0a1aa277d3d13666
marciojosebaldo <marciojosebaldo@gmail.com>
2023-08-25 20:55:24 -0300

    Add files via upload

--- src/main/java/Calculadora/Potencia/Controller/C_Calculadora/Potencia.java
+++ src/main/java/Calculadora/Potencia/Controller/C_Calculadora/Potencia.java
@@ -1,12 +1,29 @@
 package Calculadora.Potencia.Controller.C_Calculadora;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Potencia {
 
-    @GetMapping
-    
+    @GetMapping("/")
+    public String getHome() {
+        return "home";
+    }
 
+    @PostMapping("/")
+    public String calculaPotencia(@RequestParam("numero") int numero,
+                                  Model model) {
+
+        int resultado = numero * numero;
+
+        model.addAttribute("resultado", resultado);
+        model.addAttribute("numero", numero);
+
+        return "home";
+    }
 }
